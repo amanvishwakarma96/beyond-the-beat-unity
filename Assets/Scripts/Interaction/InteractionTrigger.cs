9c2c3eb107b7d3ec267b5c9e1becae6a6ac39421
amanvishwakarma96 <80578610+amanvishwakarma96@users.noreply.github.com>
2026-08-21 11:09:01 +0530

    feat: add reusable interaction trigger bridge

---
+++ Assets/Scripts/Interaction/InteractionTrigger.cs
@@ -0,0 +1,86 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BeyondTheBeat.Interaction
+{
+    [RequireComponent(typeof(Collider))]
+    public sealed class InteractionTrigger : MonoBehaviour
+    {
+        [SerializeField] private InteractableObject interactable;
+
+        private readonly Dictionary<InteractionController, int> overlapCounts = new Dictionary<InteractionController, int>(2);
+
+        private void Awake()
+        {
+            Collider trigger = GetComponent<Collider>();
+            trigger.isTrigger = true;
+
+            if (interactable == null)
+            {
+                interactable = GetComponent<InteractableObject>();
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (interactable == null)
+            {
+                return;
+            }
+
+            InteractionController controller = other.GetComponentInParent<InteractionController>();
+            if (controller == null)
+            {
+                return;
+            }
+
+            int count;
+            overlapCounts.TryGetValue(controller, out count);
+            overlapCounts[controller] = count + 1;
+
+            if (count == 0)
+            {
+                controller.Register(interactable);
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            InteractionController controller = other.GetComponentInParent<InteractionController>();
+            if (controller == null)
+            {
+                return;
+            }
+
+            int count;
+            if (!overlapCounts.TryGetValue(controller, out count))
+            {
+                return;
+            }
+
+            count--;
+            if (count <= 0)
+            {
+                overlapCounts.Remove(controller);
+                controller.Unregister(interactable);
+            }
+            else
+            {
+                overlapCounts[controller] = count;
+            }
+        }
+
+        private void OnDisable()
+        {
+            foreach (KeyValuePair<InteractionController, int> entry in overlapCounts)
+            {
+                if (entry.Key != null && interactable != null)
+                {
+                    entry.Key.Unregister(interactable);
+                }
+            }
+
+            overlapCounts.Clear();
+        }
+    }
+}
